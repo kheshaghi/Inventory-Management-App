2c0b37a05a52c0b5fbe368a528c6633f806703f8
Kheshaghi <kkheshaghi97@gmail.com>
2024-07-01 10:30:55 +0530

    login page changes

--- src/main/java/com/rest/kn/InventoryManagementApplication.java
+++ src/main/java/com/rest/kn/InventoryManagementApplication.java
@@ -1,13 +1,40 @@
 package com.rest.kn;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
+
+import javax.swing.SwingUtilities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.rest.kn.ui.Login;
 
 @SpringBootApplication
-public class InventoryManagementApplication {
+public class InventoryManagementApplication implements CommandLineRunner{
 
+	@Autowired
+	Login login;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagementApplication.class, args);
+//		SpringApplication.run(InventoryManagementApplication.class, args);
+		ApplicationContext contexto = new SpringApplicationBuilder(InventoryManagementApplication.class)
+                .web(WebApplicationType.NONE)
+                .headless(false)
+                .bannerMode(Mode.OFF)
+                .run(args);
 	}
-
+	
+	 @Override
+	    public void run(String... args) throws Exception {
+		 System.out.println(Arrays.toString(args));
+	        SwingUtilities.invokeLater(() -> {
+	        	login.setVisible(true);
+	        });
+	    }
+	 
 }
